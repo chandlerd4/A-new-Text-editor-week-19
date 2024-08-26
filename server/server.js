6aaaf2c3a26d53b8da4dc5ab0be1a2dd8fa88284
chan <chandlerdaniel254@gmail.com>
2024-08-25 21:41:24 -0400

    updated server file

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path')
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require('./routes/htmlRoutes')(app);
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
