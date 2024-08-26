41d38e9244e9d0cb633de2650cc49dfd63da7c2a
chan <chandlerdaniel254@gmail.com>
2024-08-25 21:22:41 -0400

    commit

---
+++ client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () => {
+  const db = await openDB('jate', 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains('jate')) {
+        db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
+      } else {
+        console.log('jate database already exists');
+      }
+    },
+  });
+  return db;
+};
+
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const result = await store.put({ id: 1, value: content });
+  console.log('Saved', result);
+  return result;
+};
+
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.get(1);
+  console.log('Loaded', result);
+  return result;
+};
+
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = () => ({
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    // Simplifies creation of HTML files to serve your bundles
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Just Another Text Editor',
+    }),
+    // Injects a service worker into your Webpack bundle
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'src-sw.js',
+    }),
+    // Generates a web app manifest file
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'J.A.T.E.',
+      description: 'A text editor application that functions offline.',
+      background_color: '#31a9e1',
+      theme_color: '#31a9e1',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      // CSS loader
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Babel loader for JS files
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread'
+            ],
+          },
+        },
+      },
+    ],
+  },
+});
